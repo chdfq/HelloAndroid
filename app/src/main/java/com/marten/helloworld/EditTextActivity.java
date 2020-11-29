f484f8d98225167669337bc0bc1f96faba9846ce
Marten Zhu <449061137@qq.com>
2020-11-29 13:28:00 +0800

    format code style

--- app/src/main/java/com/marten/helloworld/EditTextActivity.java
+++ app/src/main/java/com/marten/helloworld/EditTextActivity.java
@@ -1,7 +1,5 @@
 package com.marten.helloworld;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextWatcher;
@@ -10,6 +8,8 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class EditTextActivity extends AppCompatActivity {
 
